56d18aa5e91bf375eb00d9b27ae5ca4d03d9ebc0
BrunoDunay <cesardunah@gmail.com>
2025-11-27 21:21:51 -0600

    cambios

--- ecommerce-app/src/app/core/services/products/products.service.ts
+++ ecommerce-app/src/app/core/services/products/products.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, map, Observable, throwError,  } from 'rxjs';
 import { Product, ProductResponse } from '../../types/Products';
-import { environment } from '../../../../environments/environment.development';
+import { environment } from '../../../../environments/environment';
 
 export type filters = {
   q: string;
